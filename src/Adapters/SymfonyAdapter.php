5a8f344e3f1243c10ccdc2b60d8ce35b81583142
papoon <f_martins.1989@hotmail.com>
2025-08-25 16:14:51 +0100

    feat: symfony adapter and tests

--- src/Adapters/SymfonyAdapter.php
+++ src/Adapters/SymfonyAdapter.php
@@ -5,36 +5,75 @@ declare(strict_types=1);
 namespace Inspector\Adapters;
 
 use Inspector\AdapterInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SymfonyAdapter implements AdapterInterface
 {
+    protected ContainerBuilder $container;
+
+    public function __construct(ContainerBuilder $container)
+    {
+        $this->container = $container;
+    }
+
+    /** @return array<string> */
     public function getServices(): array
     {
-        return [];
+        return array_keys($this->container->getDefinitions());
     }
 
+    /** @return array<string, string> */
     public function getAliases(): array
     {
-        return [];
+        $aliases = [];
+        foreach ($this->container->getAliases() as $alias => $serviceId) {
+            $aliases[(string)$alias] = (string)$serviceId;
+        }
+        return $aliases;
     }
 
+    /** @return array<string, array{class: string|null, public: bool}> */
     public function getBindings(): array
     {
-        return [];
+        $bindings = [];
+        foreach ($this->container->getDefinitions() as $id => $definition) {
+            $bindings[$id] = [
+                'class' => $definition->getClass(),
+                'public' => $definition->isPublic(),
+            ];
+        }
+        return $bindings;
     }
 
+    /** @return array<string> */
     public function getDependencies(string $service): array
     {
-        return [];
+        if (!$this->container->hasDefinition($service)) {
+            return [];
+        }
+        $definition = $this->container->getDefinition($service);
+        $arguments = $definition->getArguments();
+        $deps = [];
+        foreach ($arguments as $arg) {
+            if (is_string($arg)) {
+                $deps[] = $arg;
+            }
+        }
+        return $deps;
     }
 
+    /** @return array<string> */
     public function getBindingHistory(string $service): array
     {
+        // Symfony does not track binding history by default
         return [];
     }
 
     public function resolve(string $service): mixed
     {
+        if ($this->container->has($service)) {
+            return $this->container->get($service);
+        }
         return null;
     }
 }
---
+++ tests/SymfonyAdapterTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Inspector\Adapters\SymfonyAdapter;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class SymfonyAdapterTest extends TestCase
+{
+    public function testGetServicesReturnsRegisteredServices(): void
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition('foo', new Definition(\stdClass::class));
+        $container->setDefinition('bar', new Definition(\DateTime::class));
+
+        $adapter = new SymfonyAdapter($container);
+        $services = $adapter->getServices();
+       
+        $this->assertContains('foo', $services);
+        $this->assertContains('bar', $services);
+        $this->assertCount(3, $services);
+    }
+
+    public function testGetAliasesReturnsAliases(): void
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition('foo', new Definition(\stdClass::class));
+        $container->setAlias('bar', 'foo');
+
+        $adapter = new SymfonyAdapter($container);
+        $aliases = $adapter->getAliases();
+
+        $this->assertArrayHasKey('bar', $aliases);
+        $this->assertSame('foo', $aliases['bar']);
+    }
+
+    public function testGetBindingsReturnsBindings(): void
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition('foo', new Definition(\stdClass::class));
+
+        $adapter = new SymfonyAdapter($container);
+        $bindings = $adapter->getBindings();
+        
+        $this->assertArrayHasKey('foo', $bindings);
+        $this->assertSame(\stdClass::class, $bindings['foo']['class']);
+        $this->assertFalse($bindings['foo']['public']);
+    }
+
+    public function testGetDependenciesReturnsArguments(): void
+    {
+        $container = new ContainerBuilder();
+        $def = new Definition(\stdClass::class);
+        $def->setArguments(['bar', 'baz']);
+        $container->setDefinition('foo', $def);
+
+        $adapter = new SymfonyAdapter($container);
+        $deps = $adapter->getDependencies('foo');
+
+        $this->assertContains('bar', $deps);
+        $this->assertContains('baz', $deps);
+    }
+
+    public function testGetBindingHistoryReturnsEmptyArray(): void
+    {
+        $container = new ContainerBuilder();
+        $adapter = new SymfonyAdapter($container);
+
+        $history = $adapter->getBindingHistory('foo');
+        $this->assertEmpty($history);
+    }
+
+    public function testResolveReturnsServiceInstance(): void
+    {
+        $container = new ContainerBuilder();
+        $def = new Definition(\stdClass::class);
+        $def->setPublic(true); // Make the service public
+        $container->setDefinition('foo', $def);
+        $container->compile();
+
+        $adapter = new SymfonyAdapter($container);
+        $resolved = $adapter->resolve('foo');
+
+        $this->assertInstanceOf(\stdClass::class, $resolved);
+    }
+
+    public function testResolveReturnsNullForUnboundService(): void
+    {
+        $container = new ContainerBuilder();
+        $adapter = new SymfonyAdapter($container);
+
+        $resolved = $adapter->resolve('not_bound');
+        $this->assertNull($resolved);
+    }
+}
